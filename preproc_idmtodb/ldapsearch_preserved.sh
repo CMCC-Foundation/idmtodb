58daa36860ecc8216cec5c76b17df73a870618ec
DekraN <marco_chiarelli@yahoo.it>
2023-12-23 15:36:35 +0100

    -MC : now referencing users' LDAP attributes by username instead of uidnumber

--- preproc_idmtodb/ldapsearch_preserved.sh
+++ preproc_idmtodb/ldapsearch_preserved.sh
@@ -6,7 +6,7 @@ echo 'users_ldap_preserved_dict={' > user_ldap_preserved_found.py
 
 for i in ${preserved_users[@]};
 do
-	ldapsearch "(uid=$i)" [options] uidnumber modifyTimestamp 2>/dev/null | grep -e uidnumber -e "modifyTimestamp:" | sed ':a;N;$!ba;s/\nmodifyTimestamp: /": "/g' | sed ':a;N;$!ba;s/Z\nuidnumber: /",\n"/g' | sed 's/uidnumber: /"/g' | sed ':a;N;$!ba;s/Z/",/g' >> user_ldap_preserved_found.py
+	ldapsearch "(uid=$i)" [options] uid modifyTimestamp 2>/dev/null | grep -e "uid:" -e "modifyTimestamp:" | sed ':a;N;$!ba;s/\nmodifyTimestamp: /": "/g' | sed ':a;N;$!ba;s/Z\nuid: /",\n"/g' | sed 's/uid: /"/g' | sed ':a;N;$!ba;s/Z/",/g' >> user_ldap_preserved_found.py
 done
 
 sed -i ':a;N;$!ba;$s/.$/\n}/' user_ldap_preserved_found.py
