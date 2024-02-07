efcafa30224487e5db1e9521cd4bad7369b34318
Marco Chiarelli <marcochiarelli@MacBook-Pro-di-Marco.local>
2024-02-07 11:42:52 +0100

    -MC : fixed a bug in dexter_fan_in.sh which didn't allow to inhibit mail notification preview to hsm when NO was inserted in notification_date DEXTER CSV field

--- gdrive_API/dexter_fan_in.sh
+++ gdrive_API/dexter_fan_in.sh
@@ -19,7 +19,7 @@ mv files/$(echo $docx_filename_out | cut -d'/' -f2) files/$docx_filename_out
 python3 upload_docx_convert_to_pdf.py files/$docx_filename_out $name_dot_surname $email $issuer >> files/$out_dir_name/drive_api_log
 cat files/$out_dir_name/drive_api_log
 
-if [[ "$notify" -eq 1 ]] || [[ ! -z "$notify" ]];
+if [[ "$notify" -eq 1 ]] #|| [[ ! -z "$notify" ]];
 then
 	./send_user_mail.sh "$(echo ${issuer:0:1} | tr '[:lower:]' '[:upper:]')""$(echo ${issuer:1:${#issuer}} | cut -d'_' -f1)" "$first" "$username" "$email" "$(echo $div | tr '[:lower:]' '[:upper:]')" "$(cat files/$out_dir_name/drive_api_log | grep 'Folder link' | tail -n1 | cut -d' ' -f3 | tr -d '\n')" "files/$out_dir_name/$(echo $docx_filename_out | cut -d'/' -f2 | cut -d'.' -f1).pdf"	"$vpn_notify" "$mach"
 fi
